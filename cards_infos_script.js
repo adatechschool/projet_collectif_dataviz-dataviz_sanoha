96a946698013f3d93cf5f6453d40ad0505c15844
HanaaBelle <hanaabelhamidi@gmail.com>
2023-03-25 16:00:47 +0100

    Added script to load personnages page content

---
+++ cards_infos_script.js
@@ -0,0 +1,67 @@
+// Partie 1 : Réalisation du Slide
+
+// Affecter à la constante "url_api" l'url de l'API "characters"
+const url_api_characters = "https://hp-api.onrender.com/api/characters";
+// Requête axios qui dit à l'url_api va chercher la donnée dans l'API
+fetch(url_api_characters).then(response => {/// Aller chercher la donnée
+    // extraire les données les mettre dans un fichier json
+    return response.json(); // extrait mes données
+}).then(datas => {
+    console.log("DATAS", datas); //on manipule la donnée
+    datas.forEach(character => { //boucle for each
+        console.log("personnages", character.name);
+        const card = `
+            <div class="card"
+            style="text-align: center">
+            <img class="card-image" src="${character.image}">
+                <div class="card-body">
+                    <h2>Name : ${character.name}</h2>
+                    <p>house : ${character.house}</p>
+                    <span>Acteur : ${character.actor}</span>
+                </div>
+            </div>
+            `;
+        document.getElementById("personnages").innerHTML += card;
+    });
+})
+
+// Affecter à la constante "url_api" l'url de l'API "characters"
+const url_api_houses = "https://hp-api.onrender.com/api/characters/house/:house";
+axios.get(url_api_houses)
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+function ajout(liste_infos) {
+
+    //const dictionnaire_infos = {};
+    //const tableau_titres_infos = ['name', 'alternate_names', 'house'];
+    //'species', 'gender', 'ancestry', 'patronus', 'wand_core' , ...
+    //for (let i = 0; i < tableau_titres_infos.length; i++) {
+    //dictionnaire_infos[tableau_titres_infos[i]] = document.createElement('h3');
+    //console.log(dictionnaire_infos);
+
+
+    //title_species.innerText = liste_infos[i].species;
+    //title_gender.innerText = liste_infos[i].gender;
+
+
+    //title_species.innerText = liste_infos[i].species;
+    //title_gender.innerText = liste_infos[i].gender;
+    //title_ancestry.innerText = liste_infos[i].ancestry;
+    //title_house.innerText = liste_infos[i].house;
+    //title_patronus.innerText = liste_infos[i].patronus;
+    //title_wand.innerText = liste_infos[i].wand.core;
+
+    //
+    //div_box_infos.replaceChildren(title_species);
+    //div_box_infos.replaceChildren(title_gender);
+    //div_box_infos.replaceChildren(title_ancestry);
+    //div_box_infos.replaceChildren(title_house);
+    //div_box_infos.replaceChildren(title_patronus);
+    //div_box_infos.replaceChildren(title_wand);
+
+}
--- slide_infos_script.js
+++ slide_infos_script.js
@@ -1,9 +1,9 @@
 // Partie 1 : Réalisation du Slide
 
 // Affecter à la constante "url_api" l'url de l'API "characters"
-const url_api = "https://hp-api.onrender.com/api/characters";
+const url_api_characters = "https://hp-api.onrender.com/api/characters";
 // Requête axios qui dit à l'url_api va chercher la donnée dans l'API
-axios.get(url_api)
+axios.get(url_api_characters)
     // Extraire les données et les mettre dans un fichier response
     .then((response) => {
         //Affichage de response
@@ -80,72 +80,3 @@ const swiper = new Swiper('.mySwiper', {
         disableOnInteraction: true,
     },
 })
-
-
-fetch(url_api).then(response => {
-    // extraire les données les mettres dans un fichier json
-    return response.json();
-}).then(datas => {
-    console.log("DATAS", datas); //on manipule la donnée
-    datas.forEach(character => { //boucle for each
-        console.log("Personnages", character.name);
-        const card = `
-            <div class="card">
-                <div class="card-image"
-                style="width: 200px;
-                height: 200px;
-                backgroud-image: url(${character.image})";
-                backgroud-size: cover">
-                </div>
-                <div class="card-body">
-                    <h2>name : ${character.name}</h2>
-                    <p>house : ${character.house}</p>
-                    <span>acteur : ${character.actor}</span>
-                </div>
-            </div>
-            `;
-        document.getElementById("Personnages").innerHTML += card;
-    });
-
-})
-
-
-axios.get('https://hp-api.onrender.com/api/characters/house/:house')
-    .then((response) => {
-        console.log(response)
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-
-function ajout(liste_infos) {
-
-
-    //const dictionnaire_infos = {};
-    //const tableau_titres_infos = ['name', 'alternate_names', 'house'];
-    //'species', 'gender', 'ancestry', 'patronus', 'wand_core' , ...
-    //for (let i = 0; i < tableau_titres_infos.length; i++) {
-    //dictionnaire_infos[tableau_titres_infos[i]] = document.createElement('h3');
-    //console.log(dictionnaire_infos);
-
-
-    //title_species.innerText = liste_infos[i].species;
-    //title_gender.innerText = liste_infos[i].gender;
-
-
-    //title_species.innerText = liste_infos[i].species;
-    //title_gender.innerText = liste_infos[i].gender;
-    //title_ancestry.innerText = liste_infos[i].ancestry;
-    //title_house.innerText = liste_infos[i].house;
-    //title_patronus.innerText = liste_infos[i].patronus;
-    //title_wand.innerText = liste_infos[i].wand.core;
-
-    //
-    //div_box_infos.replaceChildren(title_species);
-    //div_box_infos.replaceChildren(title_gender);
-    //div_box_infos.replaceChildren(title_ancestry);
-    //div_box_infos.replaceChildren(title_house);
-    //div_box_infos.replaceChildren(title_patronus);
-    //div_box_infos.replaceChildren(title_wand);
-
-}
