4cb0484f5b95aa604f52b3ddbaec91f3ac59e6a7
HanaaBelle <hanaabelhamidi@gmail.com>
2023-03-26 17:56:38 +0200

    Added changed hard coded url parsing

--- cards_infos_script.js
+++ cards_infos_script.js
@@ -8,7 +8,7 @@ fetch(url_api_characters).then(response => {/// Aller chercher la donnée
     return response.json(); // extrait mes données
 }).then(datas => {
     console.log("DATAS", datas); //on manipule la donnée
-    datas.forEach(character => { //boucle for each
+    datas.forEach(character => { // boucle for each
         console.log("personnages", character.name);
         if (character.image === "") {
             //do nothing
@@ -50,16 +50,14 @@ fetch(url_api_characters).then(response => {/// Aller chercher la donnée
 })
 
 // Fonction qui récupère l'url de la page courante :
-// http://localhost:63342/projet_sanoha_dataviz_api/personnages.html#Cedric%20Diggory
 
 function jump_anchor() {
      const url = location.href;
-     console.log(url);
-     location.href = "#" + url.substring(66);
-     console.log(location.href);
+     console.log("URL :", url);
+     console.log("index # : ", url.indexOf("#"));
+     location.href = "#" + url.substring(url.indexOf("#")+1);
+     console.log("Hashtag location : ", location.href);
      history.replaceState(null,null,url);
-    // get anchor from url:  http://localhost:63342/projet_sanoha_dataviz_api/personnages.html#Cedric%20Diggory
-    //location.href = "#Ron%20Weasley"
 }
 
 // Affecter à la constante "url_api" l'url de l'API "characters"
